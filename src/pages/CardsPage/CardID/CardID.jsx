0ed04fa4394158ecd8d38222d2728a6f150bd4f6
KartShal <emirrucom@gmail.com>
2025-08-30 21:35:55 +0500

    putRequest

--- src/pages/CardsPage/CardID/CardID.jsx
+++ src/pages/CardsPage/CardID/CardID.jsx
@@ -1,28 +1,58 @@
 import { useParams } from "react-router-dom";
 import  styles  from "./CardID.module.css";
-import { useEffect, useState } from "react";
-import { getHomeById } from "./service/service";
+import React, { useEffect, useState } from "react";
+import { getHomeById, putHomeById } from "./service/service";
 
 const CardID = () => {
     const [home, setHome] = useState();
+    const [isEdit, setIsEdit] = useState(false)
+    const [data, setData] = useState();
+
+    const nameRef = React.useRef();
+    const descRef = React.useRef();
+
+    const onChangeHandler = () => {
+        setData({ name: nameRef.current.value , desc: descRef.current.value});        
+    }
 
     const id = useParams();
-    console.log(id);
     
     useEffect(()=> {
-
         getHomeById(id.id).then((res)=> setHome(res.data))
-    }, [id])
+    },[id])
+
+    const putData = () => {
+        putHomeById(home.id, data).then((res) => setHome(res.data));
+        setIsEdit(false);
+    }
     return (
         <div className={styles.CardID}>
             { home && (
+            isEdit === false ? 
             <>
                 <h2>{home.name}</h2>
-                <img src={home.img} />
+                <img src={home.img} alt="img"/>
                 <p>{home.desc}</p>
+                <div className={styles.buttons}>
+                    <button>Удалить</button>
+                    <button onClick={()=> setIsEdit(true)}>Редактировать</button>
+                </div>
+            </>
+            : 
+            <>
+                <img src={home.img} alt="img"/>
+                <div className={styles.inputs}>
+                <input ref={nameRef} type="text" onChange={onChangeHandler} placeholder={home.name} />
+                <input onChange={onChangeHandler} ref={descRef} type="text"  placeholder={home.desc} />
+                </div>
+                <div className={styles.buttonsTrue}>
+                    <button onClick={()=> setIsEdit(false)}>Отмена</button>
+                    <button onClick={putData}>Сохранить</button>
+                </div>
             </>
             )}
         </div>
     )
 }
+
 export default CardID
--- src/pages/CardsPage/CardID/service/service.jsx
+++ src/pages/CardsPage/CardID/service/service.jsx
@@ -4,3 +4,11 @@ import { BASE_URL } from "../../../../shared/const/const"
 export const getHomeById = (id) => {
     return api(BASE_URL).get(`/home/${id}`);
 }
+
+export const getMaterialById = (id) => {
+    return api(BASE_URL).get(`/material/${id}`)
+}
+
+export const putHomeById = (id, data) => {
+    return api(BASE_URL).put(`/home/${id}`, data)
+}
